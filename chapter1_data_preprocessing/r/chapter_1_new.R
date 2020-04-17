30f2d1c5f6c406b4948156fa14361b277ef966ec
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-04-17 02:34:36 +0200

    ### 1.10.3. R - Taking care of Missing Data (Fix)

--- chapter1_data_preprocessing/r/chapter_1_new.R
+++ chapter1_data_preprocessing/r/chapter_1_new.R
@@ -6,6 +6,12 @@ dataset <- read.csv('Data.csv')
 # Handling Missing Data
 dataset$Age <- ifelse(
     is.na(dataset$Age),
-    ave(dataset&Age, FUN = function(x) mean(x, na.rm = TRUE)),
+    ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
     dataset$Age
+)
+
+dataset$Salary <- ifelse(
+    is.na(dataset$Salary),
+    ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+    dataset$Salary
 )
