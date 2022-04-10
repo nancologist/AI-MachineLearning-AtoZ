87795b175e061371bb2fa98bb2aa0e5d66567d65
nancologist <nancologist@gmail.com>
2022-04-10 19:37:55 +0200

    # 10.1.2. Logistic Regression in Python - Step 3

--- chapter10-1_logistic-regression/ch10-1_app.py
+++ chapter10-1_logistic-regression/ch10-1_app.py
@@ -2,6 +2,7 @@ import pandas
 import numpy
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import  LogisticRegression
 
 
 # Importing Dataset
@@ -18,3 +19,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
+
+
+# Training the Logistic Regression model on Training Set
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
